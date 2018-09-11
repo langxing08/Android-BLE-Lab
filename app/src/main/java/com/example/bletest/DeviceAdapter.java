1c924fcb955c011bf13375a3059d45c6c2fbe387
董海涛 <haitao.dong@w-oasis.com>
2018-09-11 14:48:48 +0800

    增加蓝牙及定位权限

---
+++ app/src/main/java/com/example/bletest/DeviceAdapter.java
@@ -0,0 +1,17 @@
+package com.example.bletest;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.clj.fastble.BleManager;
+import com.clj.fastble.data.BleDevice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeviceAdapter {
+
+
+}
--- app/src/main/java/com/example/bletest/MainActivity.java
+++ app/src/main/java/com/example/bletest/MainActivity.java
@@ -1,13 +1,151 @@
 package com.example.bletest;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.clj.fastble.BleManager;
+import com.clj.fastble.data.BleDevice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_CODE_OPEN_GPS = 1;
+    private static final int REQUEST_CODE_PERMISSION_LOCATION = 2;
+
+
+    private Button btn_scan;
+
+    private DeviceAdapter mDeviceAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_scan = (Button) findViewById(R.id.btn_scan);
+        btn_scan.setText("开始扫描");
+        btn_scan.setOnClickListener(this);
+
+
+
+
+
+        // BLE 初始化及配置
+        BleManager.getInstance().init(getApplication());
+        BleManager.getInstance()
+                .enableLog(true)
+                .setReConnectCount(1, 5000)
+                .setConnectOverTime(20000)
+                .setOperateTimeout(5000);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        showConnectedDevice();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        BleManager.getInstance().disconnectAllDevice();
+        BleManager.getInstance().destroy();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_scan:
+                if (btn_scan.getText().equals("开始扫描")) {
+                    checkPermissions();
+                } else if (btn_scan.getText().equals("停止扫描")) {
+                    BleManager.getInstance().cancelScan();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "必须同意所有的权限才能使用本程序", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initView() {
+
+    }
+
+    private void showConnectedDevice() {
+    }
+
+    private void checkPermissions() {
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bluetoothAdapter.isEnabled()) {
+            Toast.makeText(this, "请打开手机蓝牙", Toast.LENGTH_SHORT).show();
+        }
+
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.BLUETOOTH);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.BLUETOOTH_ADMIN)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.BLUETOOTH_ADMIN);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (!permissionList.isEmpty()) {
+            String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        }
+    }
+
+
+
 }
