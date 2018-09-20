c45b2d57c624035f7224169d95a3fd8862844943
董海涛 <haitao.dong@w-oasis.com>
2018-09-20 16:40:27 +0800

    添加Observer和Observable注释

--- app/src/main/java/com/example/bletest/comm/Observable.java
+++ app/src/main/java/com/example/bletest/comm/Observable.java
@@ -2,7 +2,11 @@ package com.example.bletest.comm;
 
 import com.clj.fastble.data.BleDevice;
 
-
+/**
+ * 被观察者
+ * 当Observable发生改变时就会通知Observer做相应的操作
+ * 一个Observable可以被很多Observer同时观察
+ */
 public interface Observable {
 
     void addObserver(Observer obj);
--- app/src/main/java/com/example/bletest/comm/Observer.java
+++ app/src/main/java/com/example/bletest/comm/Observer.java
@@ -2,6 +2,11 @@ package com.example.bletest.comm;
 
 import com.clj.fastble.data.BleDevice;
 
+/**
+ * 观察者
+ * 当Observable发生改变时就会通知Observer做相应的操作
+ * 一个Observable可以被很多Observer同时观察
+ */
 public interface Observer {
 
     void disConnected(BleDevice bleDevice);
